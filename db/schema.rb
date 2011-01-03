6c00d4bddf18bc09595c15db9ca340138caf03f4
Kyle Ferran <etrnly@gmail.com>
2011-01-02 19:22:50 -0700

    Finished chapter 3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110103014906) do
+ActiveRecord::Schema.define(:version => 20110103021456) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(:version => 20110103014906) do
     t.datetime "published_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "excerpt"
+    t.string   "location"
   end
 
 end
