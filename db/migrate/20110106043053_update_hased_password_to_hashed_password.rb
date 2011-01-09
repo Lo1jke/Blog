13f82d750389d653813da3374967b6fa9ca69688
Kyle Ferran <kyleferran@Kyle-Ferrans-MacBook.local>
2011-01-09 15:22:16 -0700

    updated migration file

--- db/migrate/20110106043053_update_hased_password_to_hashed_password.rb
+++
@@ -1,9 +0,0 @@
-class UpdateHasedPasswordToHashedPassword < ActiveRecord::Migration
-  def self.up
-    rename_column :users, :hased_password, :hashed_password
-  end
-
-  def self.down
-    rename_column :users, :hased_password, :hashed_password
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110106043053) do
+ActiveRecord::Schema.define(:version => 20110109222054) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
